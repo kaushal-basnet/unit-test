c89dce446adafe2a2f8b085ce0cf742c3f6cb985
Kaushal Basnet <basnetkazi2@gmail.com>
2023-06-29 08:29:10 +0545

    installing and setup msw for mocking api

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,16 @@
 import "@testing-library/jest-dom/extend-expect";
+import { server } from "./mocks/server";
+
+//before every test excute everything inside callback function
+beforeAll(() => {
+  server.listen();
+});
+
+//after each test excute everything inside callback function
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
